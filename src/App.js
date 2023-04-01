cc6f8ce8d7b6c9d72ba9742fcaa17138d5887063
Bellam Dinesh <dineshbellam98@gmail.com>
2023-04-01 13:32:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect,useState}from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [data,setData] = useState([])
+  useEffect(()=>{
+     axios.get("https://jsonplaceholder.typicode.com/users").then(response=>setData(response.data))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map(item=><li key={item.id}>{item.name}</li>)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
